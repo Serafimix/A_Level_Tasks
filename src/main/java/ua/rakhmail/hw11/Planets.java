5bd153531f4760769238eb1d1b3c267e11483b45
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-03 01:48:22 +0300

    first commit

--- src/main/java/ua/rakhmail/hw11/Planets.java
+++ src/main/java/ua/rakhmail/hw11/Planets.java
@@ -1,5 +1,16 @@
 package ua.rakhmail.hw11;
 
 public enum Planets {
-    MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN
+    MERCURY(0), VENUS(1), EARTH(1), MARS(2), JUPITER(79), SATURN(82);
+
+    private int satellite;
+
+    Planets(int i) {
+        this.satellite = i;
+    }
+
+    public int getSolar(){
+        return satellite;
+    }
+
 }
