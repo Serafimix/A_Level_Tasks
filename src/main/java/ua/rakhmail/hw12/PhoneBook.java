e0c6ae6ce7152b24190cb7b12e23c7198f73eef6
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-08 06:28:52 +0300

    test commit 4

--- src/main/java/ua/rakhmail/hw12/PhoneBook.java
+++ src/main/java/ua/rakhmail/hw12/PhoneBook.java
@@ -5,20 +5,24 @@ import java.util.Optional;
 
 class PhoneBook {
     private static final String[] PHONE_BOOK = new String[3];
-    private final String stringTrue = "016/161617";
-    private String stringNull;
+    private static final String stringTrue = "016/161617";
+    private static String stringNull;
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
         PHONE_BOOK[0] = "016/161616";
         PHONE_BOOK[1] = "016/161617";
         PHONE_BOOK[2] = "016/161618";
-        System.out.println("Index of number "+ phoneBook.stringTrue + " = " +
-                phoneBook.findIndexByPhoneNumber(phoneBook.stringTrue).get());
-        System.out.println("Search with null "+ phoneBook.findIndexByPhoneNumber(phoneBook.stringNull));
+        System.out.println("Index of number " + stringTrue + " = " +
+                phoneBook.findIndexByPhoneNumber(stringTrue));
+        System.out.println("Search with null " + phoneBook.findIndexByPhoneNumber(stringNull));
     }
 
     public Optional<Integer> findIndexByPhoneNumber(String phoneNumber) {
-        return Optional.of(Arrays.asList(PHONE_BOOK).indexOf(phoneNumber));
+        //знаю что можно это сделать одной строчкой, но я не понял как.
+        Optional<Integer> number = Optional.of(Arrays.asList(PHONE_BOOK).indexOf(phoneNumber));
+        if (number.get() != -1)
+            return number;
+        else return Optional.empty();
     }
 }
