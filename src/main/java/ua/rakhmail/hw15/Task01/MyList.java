050121c88f3237de45d1a6966fd1c910e4dcb628
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-17 12:51:20 +0300

    test

---
+++ src/main/java/ua/rakhmail/hw15/Task01/MyList.java
@@ -0,0 +1,15 @@
+package ua.rakhmail.hw15.Task01;
+
+public class MyList<E> {
+    private MyNode<E> head;
+    private MyNode<E> tail;
+
+    public void add(E value) {
+
+    }
+
+    public E getFromLast(E value) {
+
+        return null;
+    }
+}
---
+++ src/main/java/ua/rakhmail/hw15/Task01/MyNode.java
@@ -0,0 +1,7 @@
+package ua.rakhmail.hw15.Task01;
+
+public class MyNode<E> {
+    private E value;
+    private MyNode<E> next;
+    private MyNode<E> prev;
+}
