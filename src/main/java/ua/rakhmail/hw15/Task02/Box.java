7a36ef26aa433e09ccda08059e793329d6ab3e9b
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-17 16:27:11 +0300

    first commit

--- src/main/java/ua/rakhmail/hw15/Task02/Box.java
+++ src/main/java/ua/rakhmail/hw15/Task02/Box.java
@@ -1,10 +1,54 @@
 package ua.rakhmail.hw15.Task02;
 
-public class Box<E> {
-    /*
-    Создать бокс с параметром обьема
-    создать отсортированный  Set и поместить в него 5+
-    обьектов бокс
-    правила сортировки От большего к меньшему, 0 в начале списка.
-     */
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Box implements Comparable<Box> {
+    private final int value;
+
+    public static void main(String[] args) {
+        Set<Box> boxSet = new TreeSet<>(new BoxComparator());
+        fillSet(boxSet);
+        for (var boxValue : boxSet) {
+            System.out.println(boxValue.getValue());
+        }
+    }
+
+    public Box(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public int compareTo(Box b) {
+
+        return 0;
+    }
+
+    public static void fillSet(Set<Box> set) {
+        set.add(new Box(5));
+        set.add(new Box(4));
+        set.add(new Box(1));
+        set.add(new Box(0));
+        set.add(new Box(2));
+        set.add(new Box(2));
+        set.add(new Box(0));
+    }
+
+    static class BoxComparator implements Comparator<Box> {
+        @Override
+        public int compare(Box o1, Box o2) {
+            if (o1.value == 0 || o2.value == 0) {
+                if (o1.value == 0) {
+                    return -1;
+                } else return 1;
+            }
+            int dif = Integer.compare(o2.value, o1.value);
+            return dif;
+        }
+    }
 }
