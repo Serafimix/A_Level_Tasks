4fd73001ecaf132e93f809e5a280bc2d36366417
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-20 15:11:28 +0300

    second commit

--- src/test/java/ua/rakhmail/hw15/Task01/MyListTest.java
+++ src/test/java/ua/rakhmail/hw15/Task01/MyListTest.java
@@ -1,39 +1,37 @@
 package ua.rakhmail.hw15.Task01;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyListTest {
+    MyList<String> list;
 
-    MyList<Integer> myListAllBeGood;
-    MyList<Integer> myListEmpty;
-    MyList<Integer> myListEmptyForAddMethod;
-    MyList<Integer> myListOneElement;
 
     @BeforeEach
     void setUp() {
-        myListAllBeGood = new MyList<>();
-        myListOneElement = new MyList<>();
-        myListEmptyForAddMethod = new MyList<>();
-    }
-
-    @Test
-    void add() {
-    }
-
-    @Test
-    void getFromLast() {
-    }
-
-    @Test
-    void testLikeAMain() {
-        MyList<String> list = new MyList<>();
+        list = new MyList<>();
         list.add("5"); // head
         list.add("6");
         list.add("7");
         list.add("3");
         list.add("2"); // tail
-        System.out.println(list.getFromLast("3"));
+    }
+
+    @Test
+    void add() {
+        list.add("1"); //new tail.
+    }
+
+    @Test
+    void getFromLastThrowNullEx() {
+        Assert.assertThrows(NullPointerException.class, ()-> list.getFromLast("123"));
+    }
+
+    @Test
+    void testLikeAMain() {
+        Assertions.assertEquals("3", list.getFromLast("3"));
     }
 
     // проверка работоспособности.
