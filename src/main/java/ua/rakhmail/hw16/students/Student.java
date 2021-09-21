81f9841034b2b2fc11729d4b0da3efb69ebdda59
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-21 14:32:31 +0300

    test

--- src/main/java/ua/rakhmail/hw16/students/Student.java
+++ src/main/java/ua/rakhmail/hw16/students/Student.java
@@ -7,17 +7,36 @@ public class Student {
     private String name;
     private String group;
     private int course;
-    private int grade;
+    private double grade;
 
     List<Student> listOfStudents = new ArrayList<>();
-    public Student(String name, String group, int course, int grade) {
+
+    public Student(String name, String group, int course, double grade) {
         this.name = name;
         this.group = group;
-        this.course = course;
-        this.grade = grade;
+        if (course >= 1 && course <= 6) {
+            this.course = course;
+        }
+        if (grade > 5) {
+            this.grade = grade;
+        }
     }
 
-    public void kickAllDump (List<Student> list) {
+    public void kickAllDump(List<Student> list) {
+        for (var stu : list) {
+            if (stu.grade < 3) {
+                list.remove(stu);
+            } else if (stu.grade >= 3 && stu.course < 6) {
+                stu.course += 1;
+            }
+        }
+    }
 
+    public void printStudents(List<Student> students, int course){
+        for (var stu : students){
+            if (stu.course == course){
+                System.out.println(stu.name);
+            }
+        }
     }
 }
