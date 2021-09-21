d357a8554548c189a71e4618810090c3979010fd
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-21 22:00:55 +0300

    first commit

---
+++ src/main/java/ua/rakhmail/hw16/students/Main.java
@@ -0,0 +1,36 @@
+package ua.rakhmail.hw16.students;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<Student> listOfStudents = new ArrayList<>();
+        listOfStudents.add(new Student("Alesha", "Bogatyr", 1, 1.0));
+        listOfStudents.add(new Student("Dobryna", "Bogatyr", 3, 3.5));
+        listOfStudents.add(new Student("Ilia", "Bogatyr", 6, 3.1));
+        listOfStudents.add(new Student("Zmei Gorinych", "Fairytale", 2, 2.9));
+        listOfStudents.add(new Student("Tugarin", "Antogonists", 3, 4.5));
+        listOfStudents.add(new Student("Kalyvan", "Antogonists", 5, 4.9));
+        listOfStudents.add(new Student("Lubava", "Bogatyr", 4, 4.1));
+        listOfStudents.add(new Student("Alena", "Bogatyr", 6, 3.1));
+        listOfStudents.add(new Student("Knyaz", "Antogonists", 5, 2.5));
+        listOfStudents.add(new Student("Gaius Iulius Caesar", "Horse", 4, 5.0));
+        listOfStudents.add(new Student("Baba Yaga", "Fairytale", 2, 4.1));
+        listOfStudents.add(new Student("Koshei", "Fairytale", 1, 4.4));
+        listOfStudents.add(new Student("Wisp", "Fairytale", 2, 3.1));
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Course " + i);
+            Student.printStudents(listOfStudents, i);
+            System.out.println("***********************");
+        }
+        Student.kickAllDumps(listOfStudents);
+        System.out.println("/////////////////////////////////////////////////////////////");
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Course " + i);
+            Student.printStudents(listOfStudents, i);
+            System.out.println("***********************");
+        }
+    }
+}
--- src/main/java/ua/rakhmail/hw16/students/Student.java
+++ src/main/java/ua/rakhmail/hw16/students/Student.java
@@ -1,40 +1,41 @@
 package ua.rakhmail.hw16.students;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-    private String name;
-    private String group;
+    private final String name;
+    private final String group;
     private int course;
     private double grade;
 
-    List<Student> listOfStudents = new ArrayList<>();
-
-    public Student(String name, String group, int course, double grade) {
+    public Student(String name, String group, int course, double grade) throws Exception {
         this.name = name;
         this.group = group;
         if (course >= 1 && course <= 6) {
             this.course = course;
-        }
-        if (grade > 5) {
+        } else throw new Exception("There is only a course from 1 to 6");
+        if (grade <= 5 && grade >= 1) {
             this.grade = grade;
-        }
+        } else throw new Exception("There is only a grade from 1 to 5");
     }
 
-    public void kickAllDump(List<Student> list) {
-        for (var stu : list) {
-            if (stu.grade < 3) {
-                list.remove(stu);
-            } else if (stu.grade >= 3 && stu.course < 6) {
-                stu.course += 1;
+    public static void kickAllDumps(List<Student> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).grade < 3) {
+                list.remove(i);
+                i--;
+            }
+        }
+        for (Student student : list) {
+            if (student.course < 6) {
+                student.course += 1;
             }
         }
     }
 
-    public void printStudents(List<Student> students, int course){
-        for (var stu : students){
-            if (stu.course == course){
+    public static void printStudents(List<Student> students, int course) {
+        for (var stu : students) {
+            if (stu.course == course) {
                 System.out.println(stu.name);
             }
         }
