1f1f844fca159b1629430a19a671cc7b418cecbc
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-22 00:22:45 +0300

    test

--- src/main/java/ua/rakhmail/hw16/Task02.java
+++ src/main/java/ua/rakhmail/hw16/Task02.java
@@ -1,7 +1,19 @@
 package ua.rakhmail.hw16;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Task02 {
-    static HashMap<String, Integer> vocabulary = new HashMap<>();
+    static Map<String, Integer> vocabulary = new TreeMap<String, Integer>();
+
+    public static void putInVocabulary(String line){
+        vocabulary.put(line, vocabulary.containsKey(line) ? vocabulary.get(line) + 1 : 1);
+    }
+
+    public static void main(String[] args) {
+        putInVocabulary("Mom");
+        System.out.println(vocabulary.get("Mom"));
+        putInVocabulary("Mom");
+        System.out.println(vocabulary.get("Mom"));
+    }
 }
