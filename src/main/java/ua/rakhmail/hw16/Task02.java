e5edbbcf357e6b751838a4cc756c877121feb469
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-22 14:15:36 +0300

    first commit

--- src/main/java/ua/rakhmail/hw16/Task02.java
+++ src/main/java/ua/rakhmail/hw16/Task02.java
@@ -1,19 +1,37 @@
 package ua.rakhmail.hw16;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Task02 {
-    static Map<String, Integer> vocabulary = new TreeMap<String, Integer>();
+    static SortedMap<String, Integer> vocabulary = new TreeMap<>();
 
-    public static void putInVocabulary(String line){
-        vocabulary.put(line, vocabulary.containsKey(line) ? vocabulary.get(line) + 1 : 1);
+    public static void putInVocabulary(String line) {
+        if (line != null) {
+            vocabulary.put(line, vocabulary.containsKey(line) ? vocabulary.get(line) + 1 : 1);
+        }
+    }
+
+    public static void printMap(Map<String, Integer> map) {
+        map.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue())
+                .forEach(System.out::println);
     }
 
     public static void main(String[] args) {
         putInVocabulary("Mom");
-        System.out.println(vocabulary.get("Mom"));
         putInVocabulary("Mom");
-        System.out.println(vocabulary.get("Mom"));
+        putInVocabulary("Mom");
+        putInVocabulary("Mom");
+        putInVocabulary("Mom");
+        putInVocabulary("Dad");
+        putInVocabulary("Dad");
+        putInVocabulary("Dad");
+        putInVocabulary("Son");
+        putInVocabulary("Son");
+        putInVocabulary("Son");
+        putInVocabulary("Son");
+        putInVocabulary("Son");
+        putInVocabulary("Son");
+        printMap(vocabulary);
     }
 }
