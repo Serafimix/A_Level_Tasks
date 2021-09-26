563425c1f43dfc206ad6c60acafca98b07f526c2
Vitalii Rakhmail <serafimix@gmail.com>
2021-09-26 16:35:31 +0300

    after repair

--- src/main/java/ua/rakhmail/hw16/task03/Main.java
+++ src/main/java/ua/rakhmail/hw16/task03/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Integer[] integers = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         String[] strings = new String[]{"Bla", "Bla-bla", "Bla-bla-bla"};
-        IteratorForArray numbers = new IteratorForArray(integers);
+        IteratorForArray<Integer> numbers = new IteratorForArray<>(integers);
         while (numbers.hasNext()) {
             System.out.println(numbers.next());
         }
         System.out.println(System.lineSeparator() + "*************************************" + System.lineSeparator());
-        IteratorForArray stringIterator = new IteratorForArray(strings);
+        IteratorForArray<String> stringIterator = new IteratorForArray<>(strings);
         while (stringIterator.hasNext()) {
             System.out.println(stringIterator.next());
         }
