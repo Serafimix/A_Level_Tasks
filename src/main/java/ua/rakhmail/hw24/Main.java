3ee3fbf33000b77bc21a342a640f485c35e4c328
Serafimix <serafimix@gmail.com>
2021-10-20 23:13:52 +0300

    test

--- src/main/java/ua/rakhmail/hw24/Main.java
+++ src/main/java/ua/rakhmail/hw24/Main.java
@@ -13,6 +13,7 @@ public class Main {
         FactoryService factoryService = new FactoryService();
         TechniqueService techniqueService = new TechniqueService();
         createTenTechniquesAnd4factory();
+
     }
 
 
@@ -31,34 +32,34 @@ public class Main {
         factoryService.saveFactory(factory4);
 
         Technique technique1 = new Technique("telephone", "LG150", 450, "2012-02-13", "some text1", factory1);
-        factory1.getTechniques().add(technique1);
+//        factory1.getTechniques().add(technique1);
         techniqueService.saveTechnique(technique1);
         Technique technique2 = new Technique("television", "AOC32", 500, "2011-03-15", "some text2", factory2);
-        factory2.getTechniques().add(technique2);
+//        factory2.getTechniques().add(technique2);
         techniqueService.saveTechnique(technique2);
         Technique technique3 = new Technique("telephone", "iPhone10", 1000, "2010-04-21", "some text3", factory3);
-        factory3.getTechniques().add(technique3);
+//        factory3.getTechniques().add(technique3);
         techniqueService.saveTechnique(technique3);
         Technique technique4 = new Technique("television", "Berezka", 300, "1994-05-22", "some text4", factory4);
-        factory4.getTechniques().add(technique4);
+//        factory4.getTechniques().add(technique4);
         techniqueService.saveTechnique(technique4);
         Technique technique5 = new Technique("telephone", "Nokian73Me", 200, "2018-06-24", "some text5", factory1);
-        factory1.getTechniques().add(technique5);
+//        factory1.getTechniques().add(technique5);
         techniqueService.saveTechnique(technique5);
-//        Technique technique6 = new Technique("television", "SAMSUNGWatch", 600, "2011-07-11", "some text6", factory2);
+        Technique technique6 = new Technique("television", "SAMSUNGWatch", 600, "2011-07-11", "some text6", factory2);
 //        factory2.getTechniques().add(technique6);
-//        techniqueService.saveTechnique(technique6);
-//        Technique technique7 = new Technique("telephone", "SiemensA50", 100, "2013-08-01", "some text7", factory3);
+        techniqueService.saveTechnique(technique6);
+        Technique technique7 = new Technique("telephone", "SiemensA50", 100, "2013-08-01", "some text7", factory3);
 //        factory3.getTechniques().add(technique7);
-//        techniqueService.saveTechnique(technique7);
-//        Technique technique8 = new Technique("television", "iWatchPro", 1100, "2018-09-14", "some text8", factory4);
+        techniqueService.saveTechnique(technique7);
+        Technique technique8 = new Technique("television", "iWatchPro", 1100, "2018-09-14", "some text8", factory4);
 //        factory4.getTechniques().add(technique8);
-//        techniqueService.saveTechnique(technique8);
-//        Technique technique9 = new Technique("telephone", "SAMSUNGg15", 700, "2020-12-17", "some text9", factory1);
+        techniqueService.saveTechnique(technique8);
+        Technique technique9 = new Technique("telephone", "SAMSUNGg15", 700, "2020-12-17", "some text9", factory1);
 //        factory1.getTechniques().add(technique9);
-//        techniqueService.saveTechnique(technique9);
-//        Technique technique10 = new Technique("television", "ChinaTV", 300, "2005-11-30", "some text10", factory2);
+        techniqueService.saveTechnique(technique9);
+        Technique technique10 = new Technique("television", "ChinaTV", 300, "2005-11-30", "some text10", factory2);
 //        factory2.getTechniques().add(technique10);
-//        techniqueService.saveTechnique(technique10);
+        techniqueService.saveTechnique(technique10);
     }
 }
