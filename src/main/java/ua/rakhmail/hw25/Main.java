f75bb2dedeacf6fe4a8f7c5276db22734676dc2a
Serafimix <serafimix@gmail.com>
2021-10-29 00:17:42 +0300

    hw25

--- src/main/java/ua/rakhmail/hw25/Main.java
+++ src/main/java/ua/rakhmail/hw25/Main.java
@@ -22,11 +22,11 @@ public class Main {
         Vehicle vehicle1 = new Vehicle("Batgirl Cycle", "1950-05-30", 25000, TyresType.SUMMER_SEASON, "2021-01-01");
         vehicle1.setOwner(owner1);
         Vehicle vehicle2 = new Vehicle("BlackHand car", "1999-09-29", 12500, TyresType.WINTER_SEASON, "2015-02-02");
-        vehicle1.setOwner(owner2);
+        vehicle2.setOwner(owner2);
         Vehicle vehicle3 = new Vehicle("New Flash Cycle", "1988-08-08", 5000, TyresType.SUMMER_SEASON, "2018-08-08");
         vehicle3.setOwner(owner3);
         Vehicle vehicle4 = new Vehicle("Flash Cycle", "1940-07-07", 10000, TyresType.WINTER_SEASON, "2014-04-04");
-        vehicle1.setOwner(owner4);
+        vehicle4.setOwner(owner4);
         Vehicle vehicle5 = new Vehicle("Joker Bicycle", "1966-06-06", 1, TyresType.ALL_SEASON, "not date text");
         List<Vehicle> vehicles = new LinkedList<>();
         Collections.addAll(vehicles, vehicle1, vehicle2, vehicle3, vehicle4, vehicle5);
