7dc6d6e46c265581a4ea227f6cfe472a98d412cd
Serafimix <serafimix@gmail.com>
2021-11-04 23:50:53 +0200

    hw28

--- src/main/java/ua/rakhmail/hw28/HerokuApp.java
+++ src/main/java/ua/rakhmail/hw28/HerokuApp.java
@@ -2,7 +2,6 @@ package ua.rakhmail.hw28;
 
 public class HerokuApp {
     public static void main(String[] args) {
-        System.out.println("Hello, Heroku");
+        System.out.println("Hello, Herku");
     }
 }
-
