913dcf31babce6147493851d6793931eca3861ad
Serafimix <serafimix@gmail.com>
2021-11-08 15:49:14 +0200

    hw28

--- src/main/java/ua/rakhmail/hw28/Info.java
+++ src/main/java/ua/rakhmail/hw28/Info.java
@@ -2,5 +2,6 @@ package ua.rakhmail.hw28;
 
 public class Info {
     //     on another repository    https://github.com/Serafimix/heroku
-    // or                           https://mysterious-brushlands-43649.herokuapp.com/
+    //     or                       https://mysterious-brushlands-43649.herokuapp.com/
+    //     podcast task#3           https://www.youtube.com/watch?v=UrVAp1vAzsU
 }
