ea2204b8834f6e771e6ae020ee1d3e4eb61eadc7
Serafimix <serafimix@gmail.com>
2021-11-09 00:00:30 +0200

    test

--- src/main/java/ua/rakhmail/hw26/Main.java
+++ src/main/java/ua/rakhmail/hw26/Main.java
@@ -4,24 +4,9 @@ import ua.rakhmail.hw26.dao.OrderDAO;
 import ua.rakhmail.hw26.entity.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Main {
 
-
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        Iterator<String> iterator = list.iterator();
-        list.remove("2");
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-    }
 /*
 //    TESTS IS INCLUDED
 
---
+++ src/main/java/ua/rakhmail/hw29/Info.java
@@ -0,0 +1,6 @@
+package ua.rakhmail.hw29;
+
+public class Info {
+    //     on another repository    https://github.com/Serafimix/heroku
+    //     or                       https://mysterious-brushlands-43649.herokuapp.com/
+}
