9f1b612f6743909970cc0803fbb99cd1bc2526ee
Serafimix <serafimix@gmail.com>
2021-12-08 13:58:15 +0200

    add spring module

---
+++ Spring_tasks/src/test/java/ua/rakhmail/Spring_tasks/SpringTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.rakhmail.Spring_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
